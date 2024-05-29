1d4de28131cfd14c379ac98f672a652c54ec8d66
arvinxx <arvinx@foxmail.com>
2024-05-29 20:42:41 +0800

    ✨ feat: support server db with Postgres and drizzle ORM

--- src/libs/trpc/client.ts
+++ src/libs/trpc/client.ts
@@ -1,7 +1,8 @@
 import { createTRPCClient, httpBatchLink } from '@trpc/client';
 import superjson from 'superjson';
 
-import type { EdgeRouter } from '@/server/routers';
+import { fetchErrorNotification } from '@/components/FetchErrorNotification';
+import type { EdgeRouter, LambdaRouter } from '@/server/routers';
 import { createHeaderWithAuth } from '@/services/_auth';
 import { withBasePath } from '@/utils/basePath';
 
@@ -11,6 +12,50 @@ export const edgeClient = createTRPCClient<EdgeRouter>({
       headers: async () => createHeaderWithAuth(),
       transformer: superjson,
       url: withBasePath('/trpc/edge'),
+    }),
+  ],
+});
+
+export type ErrorResponse = ErrorItem[];
+
+export interface ErrorItem {
+  error: {
+    json: {
+      code: number;
+      data: Data;
+      message: string;
+    };
+  };
+}
+
+export interface Data {
+  code: string;
+  httpStatus: number;
+  path: string;
+  stack: string;
+}
+
+export const lambdaClient = createTRPCClient<LambdaRouter>({
+  links: [
+    httpBatchLink({
+      fetch: async (input, init) => {
+        const response = await fetch(input, init);
+        if (response.ok) return response;
+
+        const errorRes: ErrorResponse = await response.clone().json();
+
+        errorRes.forEach((item) => {
+          const errorData = item.error.json;
+
+          const status = errorData.data.httpStatus;
+
+          fetchErrorNotification.error({ errorMessage: errorData.message, status });
+        });
+
+        return response;
+      },
+      transformer: superjson,
+      url: '/trpc/lambda',
     }),
   ],
 });
--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -5,6 +5,14 @@ import { publicProcedure, router } from '@/libs/trpc';
 
 import { configRouter } from './edge/config';
 import { uploadRouter } from './edge/upload';
+// router that connect to db
+import { fileRouter } from './lambda/file';
+import { messageRouter } from './lambda/message';
+import { pluginRouter } from './lambda/plugin';
+import { sessionRouter } from './lambda/session';
+import { sessionGroupRouter } from './lambda/sessionGroup';
+import { topicRouter } from './lambda/topic';
+import { userRouter } from './lambda/user';
 
 export const edgeRouter = router({
   config: configRouter,
@@ -13,3 +21,16 @@ export const edgeRouter = router({
 });
 
 export type EdgeRouter = typeof edgeRouter;
+
+export const lambdaRouter = router({
+  file: fileRouter,
+  healthcheck: publicProcedure.query(() => "i'm live!"),
+  message: messageRouter,
+  plugin: pluginRouter,
+  session: sessionRouter,
+  sessionGroup: sessionGroupRouter,
+  topic: topicRouter,
+  user: userRouter,
+});
+
+export type LambdaRouter = typeof lambdaRouter;
